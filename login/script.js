037bd60fa0d3bef461d14dafbffacbfd00240c68
GiuseppeAndrade <130957683+GiuseppeAndrade@users.noreply.github.com>
2024-07-16 17:39:57 -0300

    script.js

--- login/script.js
+++ login/script.js
@@ -2,11 +2,10 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
     
     var username = document.getElementById('Usuario').value;
-    var cpf = document.getElementById('CPF').value;
     var password = document.getElementById('Senha').value;
     var message = document.getElementById('message');
 
-    if (cpf === '40155851861' && username === 'giuseppe' && password === '1234') {
+    if ( username === 'giuseppe' && password === '1234') {
         message.style.color = '#00FF00';
         message.textContent = 'Login bem-sucedido!';
     } else {
@@ -18,7 +17,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     window.location.href = 'esquecersenha.html';
 });
 document.getElementById('CriarContaBtn').addEventListener('click', function() {
-    alert('Função de criação de conta ainda não implementada.');
+    window.location.href = 'criarconta.html'
 });
 document.getElementById('MostrarSenha').addEventListener('click', function() {
     var passwordField = document.getElementById('Senha');
